ce34307a3fd6bc74ded20f9bcdb32a2f133f15d4
Indra Budi Darma <indra.budi@big.go.id>
2026-04-17 21:52:21 +0700

    tambah kolom waktu jam youtube

---
+++ prisma/migrations/20260417000000_add_waktu_jam_youtube_to_pemateri/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable profil_pemateri: add waktu, jam, youtube
+ALTER TABLE "profil_pemateri" ADD COLUMN "waktu" TEXT;
+ALTER TABLE "profil_pemateri" ADD COLUMN "jam" TEXT;
+ALTER TABLE "profil_pemateri" ADD COLUMN "youtube" TEXT;
